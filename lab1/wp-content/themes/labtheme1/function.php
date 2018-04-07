20ba5c1cb080b1da1c43e2961314b03c3fe7c3b9
bathe0603it <bathe0603it@gmail.com>
2018-04-07 07:21:57 +0700

    add home

--- lab1/wp-content/themes/labtheme1/function.php
+++ lab1/wp-content/themes/labtheme1/function.php
@@ -14,12 +14,18 @@ require_once(CORE.'/init.php');
 
 // Thiet lap chieu rong noi dung
 if (!isset($content_width)) {
-	$content_width = 620;
+	$content_width = 620; // Thiet lap noi dung bang bien content_width globals
 }
 
 /*
 * Khai bao nhung chuc nang cua theme - don gian chi la goi ham ra thoi
 */
-if (function_exists('lab1_theme_setup')) {
-	
+if (!function_exists('lab1_theme_setup')) {
+	function lab1_theme_setup(){
+
+		// Khai bao Textdomain
+		$language_folder 	= THEME_URL.'/language';	// Duong dan thuc muc language
+		load_theme_textdomain( 'lab1', $language_folder )
+	}
+	add_action( 'init', 'lab1_theme_setup' ); // thuc thi 1 ham sau khi thuc thi ham init duoc load(action hook)
 }
