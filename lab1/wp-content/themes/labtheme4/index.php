ea6658b253aaa278defb5bf63b4f4adff22250b0
Bathe0603it <32597011+Bathe0603it@users.noreply.github.com>
2018-08-21 18:56:55 +0700

    cmt

--- lab1/wp-content/themes/labtheme4/index.php
+++ lab1/wp-content/themes/labtheme4/index.php
@@ -4,32 +4,43 @@
 <div class="container">
     <div class="row">
         <div class="col-md-4">
-            <div class="well">
-                <p><a href="#">My Profile</a></p>
-                <img src="https://www.w3schools.com/bootstrap/bird.jpg" class="img-circle" height="65" width="65" alt="Avatar">
-            </div>
-            <div class="well">
-                <p><a href="#">Interests</a></p>
-                <p>
-                    <span class="label label-default">News</span>
-                    <span class="label label-primary">W3Schools</span>
-                    <span class="label label-success">Labels</span>
-                    <span class="label label-info">Football</span>
-                    <span class="label label-warning">Gaming</span>
-                    <span class="label label-danger">Friends</span>
-                </p>    
-            </div>
-            <div class="alert alert-success fade in">
-                <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
-                <p><strong>Ey!</strong></p>
-                People are looking at your profile. Find out who.
-            </div>
-            <p><a href="#">Link</a></p>
-            <p><a href="#">Link</a></p>
-            <p><a href="#">Link</a></p>
+            <?php
+                get_sidebar(); 
+            ?>
         </div>
         <div class="col-md-8">
             <div class="row">
+                <?php if (have_posts()): ?>
+                    <?php while (have_posts()): ?>
+                        <?php the_post(); ?>
+                        <!-- default -->
+
+                        <?php
+                            get_template_part( 'content', get_post_format() ); 
+                        ?>
+
+                    <?php endwhile;?>
+                    <?php 
+                        $params = array(
+                            'prev_text' => '««',    // text phan trang
+                            'next_text' => '»»',    // text phan trang
+                            'type'      => 'list',  // kieu ul li
+                            'format'    => '?page=%#%', // Dinh dang lai url kieu ?page
+                            'show_all'           => false,
+                            'mid_size'  => 2,   // so luong trang 2 ben
+                            'end_size'  => 1,   // so luong page nam cuoi cung cua phan trang page
+                        );
+                        // echo paginate_links( $params );  -- Phan trang su dung wp < 4.1
+                        // posts_nav_link(  );  // -- Phan trang su dung wp phan trang nhanh > 4.1
+                        // the_posts_pagination(  );    // -- Phan trang manh me cua wp
+
+                        // next_posts_link();
+                        the_posts_pagination();
+                        // dd($wp_query->max_num_pages);
+                    ?>
+                <?php else: ?>
+                    <?php get_template_part( 'content-none' ) ?>
+                <?php endif ?>
                 <div class="col-sm-4">
                     <div class="panel panel-primary">
                         <div class="panel-heading">BLACK FRIDAY DEAL</div>
